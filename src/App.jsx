463af04ea89757c7eae2286732517fb0d671a6aa
Animesh Mondal <animeshmondal1414@gmail.com>
2024-03-10 19:47:52 +0530

    added sidebar and overlay effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import Content from "./components/Content";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
   return (
     <div className="w-full flex justify-center items-center sm:px-28 px-6 py-10">
       <div className="w-full flex flex-col relative">
-        <Navbar />
+        <Navbar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
         <Content />
+        {sidebarOpen && (
+          <div className="fixed top-0 left-0 w-full h-full bg-black opacity-50 z-40"></div>
+        )}
       </div>
     </div>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,4 @@
-import { useState } from "react";
-import Sidebar from "./Sidebar";
-
-function Navbar() {
-  const [sidebar, setSidebar] = useState(false);
-
+function Navbar({ sidebarOpen, setSidebarOpen }) {
   return (
     <div className="w-full flex items-center justify-between mb-8">
       <img src="/images/logo.svg" alt="logo" />
@@ -11,7 +6,7 @@ function Navbar() {
         className="lg:hidden block cursor-pointer"
         src="/images/icon-menu.svg"
         alt="menu"
-        onClick={() => setSidebar(!sidebar)}
+        onClick={() => setSidebarOpen(!sidebarOpen)}
       />
       <ul className="lg:flex lg:flex-row hidden gap-10">
         <li className="text-slate-800 cursor-pointer hover:text-orange-500 ">
@@ -30,8 +25,47 @@ function Navbar() {
           Categories
         </li>
       </ul>
-      {sidebar && <Sidebar setSidebar={setSidebar} sidebar={sidebar} />}
+
+      {sidebarOpen && (
+        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+      )}
     </div>
+  );
+}
+
+function Sidebar({ sidebarOpen, setSidebarOpen }) {
+  return (
+    <aside
+      className={`fixed right-0 top-0 h-full bg-slate-100 w-64 z-50 transform ${
+        sidebarOpen ? "translate-x-0" : "translate-x-full"
+      } transition duration-300 ease-in-out`}
+    >
+      <div className="flex items-center justify-end mr-10 mt-10 mb-20">
+        <img
+          className="cursor-pointer"
+          src="/images/icon-menu-close.svg"
+          alt="close"
+          onClick={() => setSidebarOpen(!sidebarOpen)}
+        />
+      </div>
+      <ul className="px-10 flex flex-col justify-start gap-10">
+        <li className="font-semibold text-xl text-slate-800 cursor-pointer">
+          Home
+        </li>
+        <li className="font-semibold text-xl text-slate-800 cursor-pointer">
+          New
+        </li>
+        <li className="font-semibold text-xl text-slate-800 cursor-pointer">
+          Popular
+        </li>
+        <li className="font-semibold text-xl text-slate-800 cursor-pointer">
+          Trending
+        </li>
+        <li className="font-semibold text-xl text-slate-800 cursor-pointer">
+          Categories
+        </li>
+      </ul>
+    </aside>
   );
 }
 
